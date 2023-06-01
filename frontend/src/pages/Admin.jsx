5d82736ced3a2f51405c51f5ad5616a9d79affda
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-06-01 20:54:09 +0200

    Creation of base pages

---
+++ frontend/src/pages/Admin.jsx
@@ -0,0 +1,7 @@
+export default function Admin() {
+  return (
+    <section>
+      <div>Test</div>
+    </section>
+  );
+}
---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <section>
+      <div>Test</div>
+    </section>
+  );
+}
