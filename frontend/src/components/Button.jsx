3583f5e8d3ae025d05699fdf6817305f7795507d
Denis Dezest <126237877+ddZ6ii@users.noreply.github.com>
2023-06-05 10:00:02 +0200

    Update Button.jsx

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,24 +1,5 @@
-import PropTypes from "prop-types";
-
-export default function Button({ children, customStyle, onClick }) {
+export default function Button() {
   return (
-    <button type="button" className={customStyle} onClick={onClick}>
-      {children}
-    </button>
+    <button type="button"></button>
   );
 }
-
-Button.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node,
-  ]),
-  customStyle: PropTypes.string,
-  onClick: PropTypes.func,
-};
-
-Button.defaultProps = {
-  children: null,
-  customStyle: null,
-  onClick: null,
-};
