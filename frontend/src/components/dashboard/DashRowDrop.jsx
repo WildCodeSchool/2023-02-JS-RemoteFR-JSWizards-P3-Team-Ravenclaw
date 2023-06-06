157a279058a057e17bd365e186dcf206f5dcae6c
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-06-06 17:15:15 +0200

    Creation of row with dropdown

--- frontend/src/components/dashboard/DashRowDrop.jsx
+++ frontend/src/components/dashboard/DashRowDrop.jsx
@@ -1,29 +1,28 @@
+import { useState } from "react";
+
 export default function DashRowDrop() {
+  const [isToggled, setIsToggled] = useState(false);
   return (
-    <tr className="border-b dark:border-neutral">
-      <th scope="col" className="p-4">
-        <div className="flex items-center">
-          <input
-            id="checkbox-all"
-            type="checkbox"
-            className="text-primary-600 h-4 w-4 rounded border-neutral bg-transparent focus:outline-none dark:border-neutralLightest dark:bg-transparent"
-          />
-          <label htmlFor="checkbox-all" className="sr-only">
-            checkbox
-          </label>
-        </div>
-      </th>
-      <td className="whitespace-nowrap px-4 py-3 font-medium text-gray-900 dark:text-white">
-        #1100
-      </td>
-      <td className="px-4 py-3 text-sm">Title of the video</td>
-      <td className="px-4 py-3 text-sm">MOBA</td>
-      <td className="px-4 py-3 text-sm">English</td>
-      <td className=" px-4 py-3 text-sm">
-        <span className="rounded-lg bg-successLight px-4 py-1 text-success">
-          Online
-        </span>
-      </td>
-    </tr>
+    <>
+      <tr className="border-b dark:border-neutral">
+        <td>
+          <button type="button" onClick={() => setIsToggled(!isToggled)}>
+            +
+          </button>
+        </td>
+        <td className="whitespace-nowrap px-4 py-3 font-medium text-gray-900 dark:text-white">
+          #1100
+        </td>
+        <td className="px-4 py-3 text-sm">Title of the video</td>
+        <td className="px-4 py-3 text-sm">MOBA</td>
+        <td className="px-4 py-3 text-sm">English</td>
+        <td className=" px-4 py-3 text-sm">
+          <span className="rounded-lg bg-successLight px-4 py-1 text-success">
+            Online
+          </span>
+        </td>
+      </tr>
+      {isToggled && <tr>This is a test</tr>}
+    </>
   );
 }
