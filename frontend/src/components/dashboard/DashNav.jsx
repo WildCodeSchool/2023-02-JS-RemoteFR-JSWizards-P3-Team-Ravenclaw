2a9ed2a4c3cc2e64e7c0bbf59395083b4a27f690
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-06-06 17:13:51 +0200

    Creation of navigation for table

---
+++ frontend/src/components/dashboard/DashNav.jsx
@@ -0,0 +1,104 @@
+export default function DashNav() {
+  return (
+    <nav
+      className="flex flex-col items-start justify-between space-y-3 p-4 md:flex-row md:items-center md:space-y-0"
+      aria-label="Table navigation"
+    >
+      <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
+        Showing
+        <span className="font-semibold text-gray-900 dark:text-white">
+          1-10
+        </span>
+        of
+        <span className="font-semibold text-gray-900 dark:text-white">
+          1000
+        </span>
+      </span>
+      <ul className="inline-flex items-stretch -space-x-px">
+        <li>
+          <a
+            href="#row"
+            className="ml-0 flex h-full items-center justify-center rounded-l-lg border border-gray-300 bg-white px-3 py-1.5 text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          >
+            <span className="sr-only">Previous</span>
+            <svg
+              className="h-5 w-5"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </a>
+        </li>
+        <li>
+          <a
+            href="#row"
+            className="flex items-center justify-center border border-gray-300 bg-white px-3 py-2 text-sm leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          >
+            1
+          </a>
+        </li>
+        <li>
+          <a
+            href="#row"
+            className="flex items-center justify-center border border-gray-300 bg-white px-3 py-2 text-sm leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          >
+            2
+          </a>
+        </li>
+        <li>
+          <a
+            href="#row"
+            aria-current="page"
+            className="text-primary-600 bg-primary-50 border-primary-300 hover:bg-primary-100 hover:text-primary-700 z-10 flex items-center justify-center border px-3 py-2 text-sm leading-tight dark:border-gray-700 dark:bg-gray-700 dark:text-white"
+          >
+            3
+          </a>
+        </li>
+        <li>
+          <a
+            href="#row"
+            className="flex items-center justify-center border border-gray-300 bg-white px-3 py-2 text-sm leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          >
+            ...
+          </a>
+        </li>
+        <li>
+          <a
+            href="#row"
+            className="flex items-center justify-center border border-gray-300 bg-white px-3 py-2 text-sm leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          >
+            100
+          </a>
+        </li>
+        <li>
+          <a
+            href="#row"
+            className="flex h-full items-center justify-center rounded-r-lg border border-gray-300 bg-white px-3 py-1.5 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          >
+            <span className="sr-only">Next</span>
+            <svg
+              className="h-5 w-5"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
