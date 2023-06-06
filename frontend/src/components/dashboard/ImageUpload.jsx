5dc9e0dd95ec919a4508506447dad5b6bbc5363b
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-06-06 21:31:00 +0200

    Updated style of ImageUpload component

--- frontend/src/components/dashboard/ImageUpload.jsx
+++ frontend/src/components/dashboard/ImageUpload.jsx
@@ -1,35 +1,15 @@
-import { useState } from "react";
-
 export default function ImageUpload() {
-  const [selectedImage, setSelectedImage] = useState(null);
-
-  const handleImage = (e) => {
-    const file = e.target.files[0];
-    setSelectedImage(file);
-  };
-
   return (
     <div className="flex flex-col gap-1.5">
       <label htmlFor="ImageUpload" className="text-primaryLightest">
         Image Upload
       </label>
-      <div className="h-full rounded-md bg-primary p-3 text-center text-neutralLightest focus:outline-none">
-        <input
-          type="file"
-          id="ImageUpload"
-          accept="Image/*"
-          onChange={handleImage}
-        />
-      </div>
-      {selectedImage && (
-        <div>
-          <img
-            src={URL.createObjectURL(selectedImage)}
-            alt="Uploaded imgage"
-            className="h-auto max-w-full"
-          />
-        </div>
-      )}
+      <input
+        type="file"
+        id="ImageUpload"
+        accept="Image/*"
+        className="file:hover:primaryLightest file:cursor-pointer file:rounded-md file:border-none file:bg-primary file:bg-gradient-to-b file:p-3 file:text-neutralLightest"
+      />
     </div>
   );
 }
