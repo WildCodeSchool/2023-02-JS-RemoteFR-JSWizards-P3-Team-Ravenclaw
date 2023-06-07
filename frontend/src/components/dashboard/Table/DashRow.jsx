42be2fb046ea1439a092060b06ff91e4ee72b3c9
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-06-07 18:23:01 +0200

    Updated Dashrow with logic

--- frontend/src/components/dashboard/Table/DashRow.jsx
+++ frontend/src/components/dashboard/Table/DashRow.jsx
@@ -1,37 +1,36 @@
 import PropTypes from "prop-types";
+import checkRowStatus from "../../../helpers/checkRowStatus";
 
-export default function DashRow({ id, name, category, language, status }) {
+export default function DashRow({ video }) {
+  const fields = Object.keys(video);
+  const values = Object.values(video);
+  let counter = 0;
   return (
     <tr className="border-b dark:border-neutral">
-      <td className="px-4 py-3 text-sm text-primaryLightest">#{id}</td>
-      <td className="px-4 py-3 text-sm">{name}</td>
-      <td className="px-4 py-3 text-sm">{category}</td>
-      <td className="px-4 py-3 text-sm">{language}</td>
-      <td className=" px-4 py-3 text-sm">
-        {/* eslint-disable */}
-        <span
-          className={`rounded-lg px-4 py-1 ${
-            status === "Online"
-              ? "bg-successLight text-success"
-              : status === "Offline"
-              ? "bg-errorLight text-errorDark"
-              : status === "Archived"
-              ? "bg-neutralLightest text-neutralDark"
-              : null
-          }`}
-        >
-          {status}
-        </span>
-        {/* eslint-enable */}
-      </td>
+      {values.map((value, index) => {
+        counter += 1;
+        return (
+          <td key={counter} className="px-4 py-3 text-sm">
+            {typeof value === "boolean" ? (
+              <input type="checkbox" checked={value} readOnly />
+            ) : (
+              <span className={checkRowStatus(fields[index], value)}>
+                {value}
+              </span>
+            )}
+          </td>
+        );
+      })}
     </tr>
   );
 }
 
 DashRow.propTypes = {
-  id: PropTypes.number.isRequired,
-  name: PropTypes.string.isRequired,
-  category: PropTypes.string.isRequired,
-  language: PropTypes.string.isRequired,
-  status: PropTypes.string.isRequired,
+  video: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    category: PropTypes.string,
+    language: PropTypes.string,
+    status: PropTypes.string,
+  }).isRequired,
 };
