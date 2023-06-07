823daa6856f042f9d37889faba96191fc4dcceda
Thibaut Resse <thibaut.resse@outlook.fr>
2023-06-07 19:28:37 +0200

    added notification bell and prepared state for the account logo

--- frontend/src/components/Notification.jsx
+++ frontend/src/components/Notification.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 export default function Notification() {
+  const [calcul, setCalcul] = useState(0);
+
+  const changer = () => {
+    return calcul >= 15 ? setCalcul("+15") : setCalcul(calcul + 1);
+  };
   return (
-    <div>
-      <div>Test</div>
+    /* eslint-disable */
+    <div onClick={() => changer()} className="relative">
+      <img src="../assets/icon/navbar/bell.svg" alt="cloche" className="h-8" />
+      {calcul === 0 ? (
+        ""
+      ) : (
+        <div className="absolute right-0 top-0 rounded-full bg-red-600 px-1 pt-[1.2px] text-xs text-white">
+          {calcul}
+        </div>
+      )}
     </div>
   );
-}
+} /* eslint-enable */
