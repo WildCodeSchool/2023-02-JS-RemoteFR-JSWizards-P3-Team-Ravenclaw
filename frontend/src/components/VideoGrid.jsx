3d812ba031029b9048bf3a9235cfb9c43cf0a2a4
Hafsa <hafsa.aitouarraou@outlook.fr>
2023-06-08 15:48:37 +0200

    video grid done

--- frontend/src/components/VideoGrid.jsx
+++ frontend/src/components/VideoGrid.jsx
@@ -1,7 +1,91 @@
+import { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/swiper-bundle.min.css";
+import { FreeMode } from "swiper";
+
+// à remplacer par des vidéos
+const videoData = [
+  {
+    id: 1,
+    img: "../public/assets/img/games/test.png",
+    title: "",
+  },
+  {
+    id: 2,
+    img: "../public/assets/img/games/test.png",
+    title: "",
+  },
+  {
+    id: 3,
+    img: "../public/assets/img/games/test.png",
+    title: "",
+  },
+  {
+    id: 4,
+    img: "../public/assets/img/games/test.png",
+    title: "",
+  },
+  {
+    id: 5,
+    img: "../public/assets/img/games/test.png",
+    title: "",
+  },
+  {
+    id: 6,
+    img: "../public/assets/img/games/test.png",
+    title: "",
+  },
+  {
+    id: 7,
+    img: "../public/assets/img/games/test.png",
+    title: "",
+  },
+  {
+    id: 8,
+    img: "../public/assets/img/games/test.png",
+    title: "",
+  },
+  {
+    id: 9,
+    img: "../public/assets/img/games/test.png",
+    title: "",
+  },
+];
 export default function VideoGrid() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    setVideos(videoData);
+  }, []);
   return (
     <div>
-      <div>Test</div>
+      <div className="block sm:hidden">
+        <div className="container">
+          <Swiper
+            freeMode
+            grabCursor
+            modules={FreeMode}
+            className="mySwiper"
+            slidesPerView={5}
+          >
+            {videos.map((video) => (
+              <SwiperSlide key={video.id}>
+                <img src={video.img} alt="" className="p-1" />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </div>
+      <div className="hidden sm:block">
+        <div className=" mt-10 flex flex-wrap">
+          {videoData.map((video) => (
+            <div className="card m-auto mb-16 w-1/4" key={video.id}>
+              <img src={video.img} alt="" />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
