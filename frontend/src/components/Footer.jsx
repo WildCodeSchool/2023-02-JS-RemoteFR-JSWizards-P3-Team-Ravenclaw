1d15f445381884580d5c24c449f724bd4598b24b
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-06-08 12:55:23 +0200

    Updated path for proper display

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,13 +6,13 @@ export default function Footer() {
       </p>
       <div className="flex items-center gap-4">
         <a href="https://www.instagram.com/">
-          <img src="./assets/icon/socials/instagram.svg" alt="instagram" />
+          <img src="../assets/icon/socials/instagram.svg" alt="instagram" />
         </a>
         <a href="https://twitter.com/">
-          <img src="./assets/icon/socials/twitter.svg" alt="twitter" />
+          <img src="/./assets/icon/socials/twitter.svg" alt="twitter" />
         </a>
         <a href="https://www.facebook.com/">
-          <img src="./assets/icon/socials/facebook.svg" alt="facebook" />
+          <img src="../assets/icon/socials/facebook.svg" alt="facebook" />
         </a>
       </div>
     </footer>
