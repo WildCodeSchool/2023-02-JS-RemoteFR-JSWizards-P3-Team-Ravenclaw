df9d5e341fb7cc6e97e2f272c7f36e193e4f878a
Denis Dezest <denis.dezest@gmail.com>
2023-06-08 14:15:04 +0200

    Fixed console warning due to incorrect spelling of strokeWidth attribute in Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/home/Hero";
+import Caroussel from "./components/Caroussel";
 
 export default function Home() {
   return (
     <section>
       <Hero />
+      <Caroussel />
     </section>
   );
 }
