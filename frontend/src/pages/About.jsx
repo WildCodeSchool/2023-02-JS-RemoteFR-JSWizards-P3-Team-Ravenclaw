63f07c73b5a2fadcc184471e33bc911a42b0606b
Alexis Chesneau <[alexischesneau64@gmail.com](mailto:alexischesneau64@gmail.com)>
2023-06-09 15:56:55 +0200

    ajout de l'import

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,3 +1,4 @@
+import SliderKeyFigures from "../components/about/SliderKeyFigures";
 import Contact from "../components/about/Contact";
 
 export default function About() {
@@ -58,6 +59,7 @@ export default function About() {
       </article>
       <article className="flex flex-col gap-3">
         <h1>KEY FIGURES</h1>
+        <SliderKeyFigures />
         {/* Slider component */}
       </article>
       <article className="flex flex-col gap-3">
