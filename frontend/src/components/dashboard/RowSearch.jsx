f9a0cbd18ad049d45dca08309b8ac7b063d57912
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-06-15 18:10:18 +0200

    Updated function for game tab add button

--- frontend/src/components/dashboard/RowSearch.jsx
+++ frontend/src/components/dashboard/RowSearch.jsx
@@ -13,6 +13,9 @@ export default function RowSearch({ activeTab }) {
     if (activeTab === "category") {
       return "Add category";
     }
+    if (activeTab === "game") {
+      return "Add game";
+    }
     if (activeTab === "page") {
       return "Add component";
     }
