ee056f90340b42900d93a93c8b3e719803d9f382
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-06-20 16:10:33 +0200

    Creation of video page

--- frontend/src/pages/Videos.jsx
+++ frontend/src/pages/Videos.jsx
@@ -1,7 +1,32 @@
+import Dropdown from "../components/utilities/Dropdown";
+import Searchbar from "../components/utilities/Searchbar";
+import Label from "../components/utilities/Label";
+
 export default function Videos() {
   return (
     <section>
-      <div>Videos</div>
+      <div className="flex w-full gap-4">
+        <span className="flex flex-col gap-1.5">
+          <Label title="Filter by category" className="text-sm" />
+          <Dropdown title="Categories" />
+        </span>
+        <span className="flex flex-col gap-1.5">
+          <Label title="Filter by games" className="text-sm" />
+          <Dropdown title="Games" />
+        </span>
+        <span className="flex w-full flex-col justify-end">
+          <Searchbar className="relative" />
+        </span>
+      </div>
+      <article>
+        <h1>Premium Videos</h1>
+      </article>
+      <article>
+        <h1>Freemium Videos</h1>
+      </article>
+      <article>
+        <h1>Favorites Videos</h1>
+      </article>
     </section>
   );
 }
