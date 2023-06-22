a59e78641e1c9b017a804386fa4ac1995216a664
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-06-22 17:47:15 +0200

    Creation of the full mobal form for new videos

--- frontend/src/components/dashboard/video/ModalVideo.jsx
+++ frontend/src/components/dashboard/video/ModalVideo.jsx
@@ -6,8 +6,32 @@ import styles from "../../../css/Table.module.css";
 
 // component
 import Button from "../../utilities/Button";
+import Input from "../../utilities/Input";
+import Label from "../../utilities/Label";
+import Dropdown from "../../utilities/Dropdown";
+
+// Data
+import games from "../../../data/games.json";
 
 export default function ModalVideo({ open, onOk, onCancel, onClick }) {
+  const language = [
+    { id: 1, name: "English" },
+    { id: 2, name: "French" },
+    { id: 3, name: "Italian" },
+    { id: 4, name: "Korean" },
+    { id: 5, name: "German" },
+    { id: 6, name: "Spanish" },
+  ];
+
+  const category = [
+    { id: 1, name: "FPS" },
+    { id: 2, name: "Action" },
+    { id: 3, name: "MOBA" },
+    { id: 4, name: "Racing" },
+    { id: 5, name: "Table game" },
+    { id: 6, name: "Sport" },
+  ];
+
   return (
     <Modal
       centered
@@ -34,7 +58,78 @@ export default function ModalVideo({ open, onOk, onCancel, onClick }) {
         </Button>,
       ]}
     >
-      <p>Some contents...</p>
+      <form>
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-wrap justify-between gap-y-4 md:flex-nowrap">
+            <Input
+              htmlFor="title"
+              title="Video Name"
+              type="text"
+              className={`${styles.input__style}`}
+              placeholder="Type video name"
+            />
+            <Input
+              htmlFor="premium-video"
+              className="m-3.5"
+              title="Premium"
+              type="checkbox"
+            />
+            <div className="flex flex-col gap-1.5">
+              <Label
+                htmlFor="game"
+                className={`${styles.label__style}`}
+                title="Game"
+              />
+              <Dropdown title="Select game" items={games} />
+            </div>
+          </div>
+          <div className="flex flex-wrap justify-between gap-4 md:flex-nowrap">
+            <div className="flex flex-col gap-1.5">
+              <Label
+                htmlFor="language"
+                className={`${styles.label__style}`}
+                title="Language"
+              />
+              <Dropdown title="Select Language" items={language} />
+            </div>
+            <div className="flex flex-col gap-1.5">
+              <Label
+                htmlFor="category"
+                className={`${styles.label__style}`}
+                title="Category"
+              />
+              <Dropdown title="Select Game Category" items={category} />
+            </div>
+          </div>
+          <div className="flex w-full flex-col gap-1.5">
+            <Label
+              htmlFor="description"
+              className={`${styles.label__style}`}
+              title="Video description"
+            />
+            <textarea
+              type="text"
+              className={`${styles.input__style} h-full w-full`}
+              placeholder="Type video description"
+            />
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <Input
+              title="Video Upload"
+              type="file"
+              accept="video/*"
+              className="file:hover:primaryLightest file:cursor-pointer file:rounded-md file:border-none file:bg-primary file:p-3 file:text-neutralLightest"
+            />
+            <Input
+              title="Image Upload"
+              type="file"
+              accept="image/*"
+              className="file:hover:primaryLightest file:cursor-pointer file:rounded-md file:border-none file:bg-primary file:p-3 file:text-neutralLightest"
+            />
+          </div>
+        </div>
+      </form>
     </Modal>
   );
 }
