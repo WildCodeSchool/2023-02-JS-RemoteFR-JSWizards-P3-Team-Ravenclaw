1a9e3595e0c0cea80f014837e7d496f76336c5b3
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-06-23 12:03:24 +0200

    Fixed display error from main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
-// Style
-import "./App.css";
-
 // Components
 import App from "./App";
+
+// Style
+import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
