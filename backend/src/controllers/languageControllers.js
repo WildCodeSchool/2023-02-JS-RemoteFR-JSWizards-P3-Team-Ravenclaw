b1e029a2805a4f3a38be67a11504b64d301695bd
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-07-03 17:46:07 +0200

    Added manager and controller for language table

---
+++ backend/src/controllers/languageControllers.js
@@ -0,0 +1,15 @@
+const models = require("../models");
+
+const getAll = async (req, res) => {
+  try {
+    const [result] = await models.language.findAll();
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .send("oops...an error occured when retrieving language from database");
+  }
+};
+
+module.exports = { getAll };
