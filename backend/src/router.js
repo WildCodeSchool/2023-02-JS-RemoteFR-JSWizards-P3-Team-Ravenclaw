1a45efad0dde63dccfca43384383ea29aeb29af4
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-07-04 17:48:35 +0200

    Update of backend routes

--- backend/src/router.js
+++ backend/src/router.js
@@ -6,12 +6,6 @@ const videoControllers = require("./controllers/videoControllers");
 const languageControllers = require("./controllers/languageControllers");
 const gameControllers = require("./controllers/gameControllers");
 
-// router.get("/items", itemControllers.browse);
-// router.get("/items/:id", itemControllers.read);
-// router.put("/items/:id", itemControllers.edit);
-// router.post("/items", itemControllers.add);
-// router.delete("/items/:id", itemControllers.destroy);
-
 router.get("/videos", videoControllers.getAll);
 router.get("/languages", languageControllers.getAll);
 router.get("/games", gameControllers.getAll);
