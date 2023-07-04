45cdc3733fb222b2069ddfad4f6189cf9b77545a
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-07-04 12:42:51 +0200

    Updated SliderGame with backend/frontend connection logic

--- frontend/src/components/home/SliderGame.jsx
+++ frontend/src/components/home/SliderGame.jsx
@@ -1,7 +1,6 @@
-// Package
+// Packages
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
-import axios from "axios";
 
 // Style
 import styles from "../../css/Slider.module.css";
@@ -9,35 +8,41 @@ import styles from "../../css/Slider.module.css";
 // Components
 import Card from "../utilities/Card";
 
-// Data
-// import games from "../../data/games.json";
-
-export default function SliderGame() {
-  const [videos, setVideos] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("/videos")
-      .then((res) => setVideos(res.data))
-      .catch((err) => console.error("Error fetching video data:", err));
-  }, []);
-
+export default function SliderGame({ games }) {
   return (
-    <ul className={`${styles.slider} ${styles.slider__game}`}>
-      {videos.map((game) => (
-        <li key={game.id}>
-          <Link to={`/videos?game=${game.name.toLowerCase()}`}>
-            <Card
-              classCSS={`${styles.card} ${styles.card__game} bg-cover`}
-              styleCSS={{
-                backgroundImage: `url(${game.thumbnail})`,
-              }}
-            >
-              <h2>{game.name}</h2>
-            </Card>
-          </Link>
-        </li>
-      ))}
-    </ul>
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    <>
+      {games.length && (
+        <ul className={`${styles.slider} ${styles.slider__game}`}>
+          {games.map((game) => (
+            <li key={game.id}>
+              <Link to={`/videos?game=${game.name.toLowerCase()}`}>
+                <Card
+                  classCSS={`${styles.card} ${styles.card__game} bg-cover`}
+                  styleCSS={{
+                    backgroundImage: `url(${game.thumbnail})`,
+                  }}
+                >
+                  <h2>{game.name}</h2>
+                </Card>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 }
+
+SliderGame.propTypes = {
+  games: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+    })
+  ),
+};
+
+SliderGame.defaultProps = {
+  games: null,
+};
