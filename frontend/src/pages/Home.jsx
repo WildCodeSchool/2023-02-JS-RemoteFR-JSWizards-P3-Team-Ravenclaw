70845a420f3ec5221dbf9937bfea0df476085e15
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-07-04 17:49:32 +0200

    Update of home page with promoted videos and games

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,38 +5,37 @@ import { useState, useEffect } from "react";
 import useAxios from "../hooks/useAxios";
 
 // Style
-// import styles from "../css/Slider.module.css";
+import styles from "../css/Slider.module.css";
 
 // Components
 import Hero from "../components/home/Hero";
 import SliderGame from "../components/home/SliderGame";
-// import SliderVideo from "../components/utilities/SliderVideo";
+import SliderVideo from "../components/utilities/SliderVideo";
 import Partners from "../components/home/Partners";
 import Footer from "../components/utilities/Footer";
-import LoaderFullPage from "../components/utilities/LoaderFullPage";
+import Loader from "../components/utilities/Loader";
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
 
   const baseUrl = import.meta.env.VITE_BACKEND_URL;
 
+  // fetch data fron databse using custom hook
   const { data: games, isLoading: isGameLoading } = useAxios(
     `${baseUrl}/games`
   );
-
-  // const { data: popularVideos, isLoading: isPopularLoading } = useAxios(
-  //   `${baseUrl}/videos`
-  // );
+  const { data: promotedVideos, isLoading: isPromotedLoading } = useAxios(
+    `${baseUrl}/videos?isPromoted=1`
+  );
 
   useEffect(() => {
-    if (!isGameLoading) setIsLoading(false);
-    // if (!isPopularLoading) setIsLoading(false);
+    if (!isGameLoading && !isPromotedLoading) setIsLoading(false);
   }, []);
 
   return (
     <>
       {isLoading ? (
-        <LoaderFullPage />
+        <Loader />
       ) : (
         <>
           <Hero />
@@ -45,15 +44,24 @@ export default function Home() {
               <h1>Games</h1>
               <SliderGame games={games} />
             </article>
-            {/* <article>
-              <h1>Popular Videos</h1>
+            <article>
+              <h1>Promoted Videos</h1>
               <SliderVideo
                 customClassSlider={styles.slider__video}
                 customClassCard={styles.card__video}
                 customClassOverlayWrapper={styles.overlay__wrapper__grid}
-                videos={popularVideos}
+                videos={promotedVideos}
               />
-            </article> */}
+            </article>
+            <article>
+              <h1>Popular Videos</h1>
+              {/* <SliderVideo
+                customClassSlider={styles.slider__video}
+                customClassCard={styles.card__video}
+                customClassOverlayWrapper={styles.overlay__wrapper__grid}
+                videos={popularVideos}
+              /> */}
+            </article>
           </section>
           <Partners />
         </>
