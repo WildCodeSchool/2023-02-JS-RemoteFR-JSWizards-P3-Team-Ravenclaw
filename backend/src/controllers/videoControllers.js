5e0ef47ac9e147934c6206021844c662a9d8c7a7
Denis Dezest <denis.dezest@gmail.com>
2023-07-05 11:20:13 +0200

    Added route to get video by id

--- backend/src/controllers/videoControllers.js
+++ backend/src/controllers/videoControllers.js
@@ -1,22 +1,35 @@
 const models = require("../models");
 const handleVideoQuery = require("../services/handleVideoQuery");
 
-const getAll = async (req, res) => {
+const getAllWiltFilters = async (req, res) => {
   try {
-    // destructure only if the client request contains a query
+    // handle query filters from client request (if any)
     const [sql, sqlDependencies] = handleVideoQuery(req.query);
 
-    const [videos] = await models.video.filterVideosByGame(
+    const [videos] = await models.video.findAllWithFilters(
       sql,
       sqlDependencies
     );
     res.json(videos);
-  } catch (error) {
-    console.error(error);
+  } catch (err) {
+    console.error(err);
     res
       .status(500)
       .send("oops...an error occured when retrieving videos from database");
   }
 };
 
-module.exports = { getAll };
+const getById = async (req, res) => {
+  try {
+    const [[video]] = await models.video.find(req.params.id);
+    if (!video) return res.status(404).send("video not found...");
+    res.json(video);
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .send("oops...an error occured when retrieving video from database");
+  }
+};
+
+module.exports = { getAllWiltFilters, getById };
--- backend/src/router.js
+++ backend/src/router.js
@@ -6,7 +6,9 @@ const videoControllers = require("./controllers/videoControllers");
 const languageControllers = require("./controllers/languageControllers");
 const gameControllers = require("./controllers/gameControllers");
 
-router.get("/videos", videoControllers.getAll);
+router.get("/videos", videoControllers.getAllWiltFilters);
+router.get("/videos/:id", videoControllers.getById);
+
 router.get("/languages", languageControllers.getAll);
 router.get("/games", gameControllers.getAll);
 
