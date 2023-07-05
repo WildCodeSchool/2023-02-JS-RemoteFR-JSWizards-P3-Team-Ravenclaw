ad6ca99107cb7d387374179aa134a81664c6217f
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-07-05 10:15:43 +0200

    Update of rowsearch with modals for each tab

--- frontend/src/components/dashboard/RowSearch.jsx
+++ frontend/src/components/dashboard/RowSearch.jsx
@@ -7,6 +7,9 @@ import Searchbar from "../utilities/Searchbar";
 import Button from "../utilities/Button";
 import Dropdown from "../utilities/Dropdown";
 import ModalVideo from "./video/ModalVideo";
+import ModalCategory from "./category/ModalCategory";
+import ModalLanguage from "./language/ModalLanguage";
+import ModalGame from "./game/ModalGame";
 
 export default function RowSearch({ activeTab }) {
   const [open, setOpen] = useState(false);
@@ -46,13 +49,34 @@ export default function RowSearch({ activeTab }) {
       );
     }
     if (activeTab === "category") {
-      return "Add category";
+      return (
+        <ModalCategory
+          open={open}
+          onOk={() => setOpen(false)}
+          onCancel={() => setOpen(false)}
+          onClick={() => setOpen(false)}
+        />
+      );
     }
     if (activeTab === "language") {
-      return "Add language";
+      return (
+        <ModalLanguage
+          open={open}
+          onOk={() => setOpen(false)}
+          onCancel={() => setOpen(false)}
+          onClick={() => setOpen(false)}
+        />
+      );
     }
     if (activeTab === "game") {
-      return "Add game";
+      return (
+        <ModalGame
+          open={open}
+          onOk={() => setOpen(false)}
+          onCancel={() => setOpen(false)}
+          onClick={() => setOpen(false)}
+        />
+      );
     }
     if (activeTab === "page") {
       return "Add component";
@@ -68,29 +92,31 @@ export default function RowSearch({ activeTab }) {
           {activeTab === "video" && <Dropdown title="Search filters" />}
         </div>
       </div>
-      <div className="flex w-full justify-center space-y-2 md:w-auto md:flex-row md:items-center md:space-x-3 md:space-y-0">
-        <Button
-          type="button"
-          customCSS="flex items-center justify-between rounded-lg bg-primary px-4 py-3 text-center text-sm text-white hover:bg-primaryLight focus:outline-none gap-2"
-          onClick={() => showModal(true)}
-        >
-          <svg
-            className="flex h-4 w-4 justify-end"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
+      {activeTab !== "dashboard" && (
+        <div className="flex w-full justify-center space-y-2 md:w-auto md:flex-row md:items-center md:space-x-3 md:space-y-0">
+          <Button
+            type="button"
+            customCSS="flex items-center justify-between rounded-lg bg-primary px-4 py-3 text-center text-sm text-white hover:bg-primaryLight focus:outline-none gap-2"
+            onClick={() => showModal(true)}
           >
-            <path
-              clipRule="evenodd"
-              fillRule="evenodd"
-              d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
-            />
-          </svg>
-          {addButton()}
-        </Button>
-        {addModal()}
-      </div>
+            <svg
+              className="flex h-4 w-4 justify-end"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                clipRule="evenodd"
+                fillRule="evenodd"
+                d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+              />
+            </svg>
+            {addButton()}
+          </Button>
+          {addModal()}
+        </div>
+      )}
     </div>
   );
 }
