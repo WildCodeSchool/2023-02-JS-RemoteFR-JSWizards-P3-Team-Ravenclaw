05e89c506e995a1342be794b6c7c8ecd232b939f
Denis Dezest <denis.dezest@gmail.com>
2023-07-07 18:37:48 +0200

    Fixed fetch database data for VideoPlayer component

--- frontend/src/components/video/Player.jsx
+++ frontend/src/components/video/Player.jsx
@@ -23,30 +23,13 @@ Player.propTypes = {
   video: PropTypes.shape({
     id: PropTypes.number,
     title: PropTypes.string,
-    game: PropTypes.string,
-    category: PropTypes.string,
-    language: PropTypes.string,
+    upload_date: PropTypes.string,
+    description: PropTypes.string,
+    slug: PropTypes.string,
     status: PropTypes.string,
-    visible: PropTypes.bool,
-    video: PropTypes.string,
-    thumbnail_game: PropTypes.string,
-    thumbnail_video: PropTypes.string,
+    thumbnail: PropTypes.string,
     url_video: PropTypes.string,
+    game_id: PropTypes.number,
+    language_id: PropTypes.number,
   }),
-};
-
-Player.defaultProps = {
-  video: PropTypes.shape({
-    id: null,
-    title: null,
-    game: null,
-    category: null,
-    language: null,
-    status: null,
-    visible: null,
-    video: null,
-    thumbnail_game: null,
-    thumbnail_video: null,
-    url_video: null,
-  }),
-};
+}.isRequired;
