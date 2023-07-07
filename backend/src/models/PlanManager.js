74953c1ed798ca426aa45d5c6985ec055fa6fa22
Hafsa <hafsa.aitouarraou@outlook.fr>
2023-07-07 11:10:54 +0200

    deleted dead code

--- backend/src/models/PlanManager.js
+++ backend/src/models/PlanManager.js
@@ -4,10 +4,6 @@ class PlanManager extends AbstractManager {
   constructor() {
     super({ table: "plan" });
   }
-
-  //   findAllWithFilters(sql, sqlDependencies) {
-  //     return this.database.query(sql, [sqlDependencies]);
-  //   }
 }
 
 module.exports = PlanManager;
