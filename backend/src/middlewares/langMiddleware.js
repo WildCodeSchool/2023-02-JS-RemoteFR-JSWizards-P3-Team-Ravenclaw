01b08357c447ca84122dbe06a223532132e24a6e
Denis Dezest <denis.dezest@gmail.com>
2023-07-12 14:34:33 +0200

    Added middleware to check for already existing language prior adding to database
    Co-authored-by: Arkantik <95821033+Arkantik@users.noreply.github.com>

---
+++ backend/src/middlewares/langMiddleware.js
@@ -0,0 +1,32 @@
+const models = require("../models");
+
+/**
+ * @desc check if language already exists prior adding it to database
+ */
+const checkForExistingLanguage = async (req, res, next) => {
+  try {
+    if (!Object.keys(req.body).length)
+      return res.status(400).send("Bad request. Body cannot be empty...");
+
+    const [languages] = await models.language.findAll();
+
+    if (!languages.length) return res.status(404).send("No languages found");
+
+    const match = languages.find(
+      (language) => language.name.toLowerCase() === req.body.name.toLowerCase()
+    );
+
+    if (match) return res.status(409).send("Language already exists!");
+
+    return next();
+  } catch (err) {
+    console.error(err);
+    return res
+      .status(500)
+      .send(
+        "oops...an error occured when checking for already existing language..."
+      );
+  }
+};
+
+module.exports = checkForExistingLanguage;
