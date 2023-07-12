e4e8637125bc59d5ec29f3e50388a0e51d24026f
Denis Dezest <denis.dezest@gmail.com>
2023-07-12 10:20:21 +0200

    Added route, controller and manager for updating language
    Co-authored-by: Arkantik <95821033+Arkantik@users.noreply.github.com>

--- backend/src/models/LanguageManager.js
+++ backend/src/models/LanguageManager.js
@@ -4,6 +4,12 @@ class LanguageManager extends AbstractManager {
   constructor() {
     super({ table: "language" });
   }
+
+  update(body, id) {
+    const { name } = body;
+    const SQL = `UPDATE ${this.table} SET name = ? WHERE id = ?`;
+    return this.database.query(SQL, [name, id]);
+  }
 }
 
 module.exports = LanguageManager;
--- backend/src/routers/languageRouter.js
+++ backend/src/routers/languageRouter.js
@@ -1,15 +1,17 @@
 const express = require("express");
 
 const languageController = require("../controllers/languageController");
+const validateLanguageInfo = require("../middlewares/validators/languageValidator");
 
 const router = express.Router();
 
 /**
- * TODO: POST handler with validateLanguage middleware...
- * TODO: PUT handler with validateLanguage middleware...
+ * TODO: POST handler with validateLanguage middleware...cl
  * TODO: DELETE handler...
  * TODO: add authentication wall...
  */
 router.get("/", languageController.getAll);
+
+router.put("/:id", validateLanguageInfo, languageController.editById);
 
 module.exports = router;
