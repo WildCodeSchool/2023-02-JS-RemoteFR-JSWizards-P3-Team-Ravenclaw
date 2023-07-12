96825777fe740c5f422987aa6cb2cd75405b8ea1
Denis Dezest <denis.dezest@gmail.com>
2023-07-12 08:55:22 +0200

    Replaced data JSON with data from database in ManageContent component, added Loader component

--- frontend/src/components/dashboard/ManageContent.jsx
+++ frontend/src/components/dashboard/ManageContent.jsx
@@ -1,7 +1,10 @@
 // Packages
+import PropTypes from "prop-types";
 import { useEffect, useState } from "react";
 import { Pagination, ConfigProvider } from "antd";
-import PropTypes from "prop-types";
+
+// Hooks
+import useAxios from "../../hooks/useAxios";
 
 // Components
 import RowSearch from "./RowSearch";
@@ -11,23 +14,31 @@ import RowVideo from "./video/RowVideo";
 import RowCategory from "./category/RowCategory";
 import RowLanguage from "./language/RowLanguage";
 import RowGame from "./game/RowGame";
-
-// Data
-import games from "../../data/game.json";
-import languages from "../../data/language.json";
-import categories from "../../data/category.json";
+import Loader from "../utilities/Loader";
 
 export default function DashTable({ videos }) {
+  const [isLoading, setIsLoading] = useState(true);
   const [activeTab, setActiveTab] = useState("video");
   const setActiveTabItem = (tab) => {
     setActiveTab(tab);
   };
-
   const [currentPage, setCurrentPage] = useState(1);
   const [pageSize, setPageSize] = useState(5);
   const [objectNumber, setObjectNumber] = useState(null);
+
   const offset = pageSize * currentPage - pageSize; // pages parcourues
   const nextPage = offset + pageSize;
+
+  const { data: games, isLoading: isGamesLoading } = useAxios("/games");
+  const { data: languages, isLoading: isLanguagesLoading } =
+    useAxios("/languages");
+  const { data: categories, isLoading: isCategoriesLoading } =
+    useAxios("/categories");
+
+  useEffect(() => {
+    if (!isLanguagesLoading && !isGamesLoading && !isCategoriesLoading)
+      setIsLoading(false);
+  }, []);
 
   useEffect(() => {
     setCurrentPage(1);
@@ -45,61 +56,67 @@ export default function DashTable({ videos }) {
       <h1>Manage Content</h1>
       <div className="relative min-w-[600px] overflow-hidden bg-white shadow-md dark:bg-gray-800 sm:rounded-lg">
         <NavTab setActiveTabItem={setActiveTabItem} />
-        <RowSearch activeTab={activeTab} />
-        <table className="w-full overflow-x-auto text-left text-base text-neutralDarkest dark:text-neutralLightest">
-          <RowHead activeTab={activeTab} />
-          <tbody>
-            {/* eslint-disable */}
-            {activeTab === "video"
-              ? videos
-                  .slice(offset, nextPage)
-                  .map((video) => <RowVideo key={video.id} video={video} />)
-              : activeTab === "category"
-              ? categories
-                  .slice(offset, nextPage)
-                  .map((category) => (
-                    <RowCategory key={category.id} category={category} />
-                  ))
-              : activeTab === "language"
-              ? languages
-                  .slice(offset, nextPage)
-                  .map((language) => (
-                    <RowLanguage key={language.id} language={language} />
-                  ))
-              : activeTab === "game"
-              ? games
-                  .slice(offset, nextPage)
-                  .map((game) => <RowGame key={game.id} game={game} />)
-              : null}
-            {/* eslint-enable */}
-          </tbody>
-        </table>
-        <ConfigProvider
-          theme={{
-            token: {
-              colorPrimary: "#9596FB",
-              colorText: "#9596FB",
-              colorBgContainer: "#1f2937",
-              colorBgTextHover: "#374151",
-              colorTextPlaceholder: "#9596FB",
-              colorBorder: "#9596FB",
-              controlOutlineWidth: "0",
-            },
-          }}
-        >
-          <Pagination
-            pageSizeOptions={[5, 10, 20, 50, 100]}
-            className="py-2 text-center"
-            pageSize={pageSize}
-            current={currentPage}
-            total={objectNumber}
-            onChange={(pageClicked, onPageSize) => {
-              setCurrentPage(pageClicked);
-              setPageSize(onPageSize);
-            }}
-            showSizeChanger
-          />
-        </ConfigProvider>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <>
+            <RowSearch activeTab={activeTab} />
+            <table className="w-full overflow-x-auto text-left text-base text-neutralDarkest dark:text-neutralLightest">
+              <RowHead activeTab={activeTab} />
+              <tbody>
+                {/* eslint-disable */}
+                {activeTab === "video"
+                  ? videos
+                      .slice(offset, nextPage)
+                      .map((video) => <RowVideo key={video.id} video={video} />)
+                  : activeTab === "category"
+                  ? categories
+                      .slice(offset, nextPage)
+                      .map((category) => (
+                        <RowCategory key={category.id} category={category} />
+                      ))
+                  : activeTab === "language"
+                  ? languages
+                      .slice(offset, nextPage)
+                      .map((language) => (
+                        <RowLanguage key={language.id} language={language} />
+                      ))
+                  : activeTab === "game"
+                  ? games
+                      .slice(offset, nextPage)
+                      .map((game) => <RowGame key={game.id} game={game} />)
+                  : null}
+                {/* eslint-enable */}
+              </tbody>
+            </table>
+            <ConfigProvider
+              theme={{
+                token: {
+                  colorPrimary: "#9596FB",
+                  colorText: "#9596FB",
+                  colorBgContainer: "#1f2937",
+                  colorBgTextHover: "#374151",
+                  colorTextPlaceholder: "#9596FB",
+                  colorBorder: "#9596FB",
+                  controlOutlineWidth: "0",
+                },
+              }}
+            >
+              <Pagination
+                pageSizeOptions={[5, 10, 20, 50, 100]}
+                className="py-2 text-center"
+                pageSize={pageSize}
+                current={currentPage}
+                total={objectNumber}
+                onChange={(pageClicked, onPageSize) => {
+                  setCurrentPage(pageClicked);
+                  setPageSize(onPageSize);
+                }}
+                showSizeChanger
+              />
+            </ConfigProvider>
+          </>
+        )}
       </div>
     </div>
   );
