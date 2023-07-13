61688dd1f3f448ad6da5b1a20a2f8fb049188822
Denis Dezest <denis.dezest@gmail.com>
2023-07-13 11:13:32 +0200

    Created validateThumbnailInfo to check the image type prior to upload
    Co-authored-by: Arkantik <95821033+Arkantik@users.noreply.github.com>

---
+++ backend/src/middlewares/validators/thumbnailValidator.js
@@ -0,0 +1,29 @@
+/**
+ * @desc automated method for server-side data validation (prior to database entry)
+ */
+const Joi = require("joi");
+
+const thumbnailSchema = Joi.object({
+  originalname: Joi.string()
+    .regex(/([A-z0-9-_./]+)?.(png|jpg|jpeg|webp)$/i)
+    .max(255)
+    .required(),
+});
+
+const validateThumbnailInfo = (req, res, next) => {
+  if (!req.file)
+    return res.status(400).send("Bad request. File cannot be empty...");
+
+  const { originalname } = req.file;
+
+  const { error } = thumbnailSchema.validate(
+    { originalname },
+    { abortEarly: false }
+  );
+
+  if (error) return res.status(422).json({ validationErrors: error.details });
+
+  return next();
+};
+
+module.exports = validateThumbnailInfo;
