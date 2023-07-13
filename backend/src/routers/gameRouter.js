58130b1264e7a5dc0136c3b9b982b4083feadcde
Denis Dezest <denis.dezest@gmail.com>
2023-07-13 08:48:33 +0200

    Created endpoints, controller handlers, model handlers and data verification middleware for Games

--- backend/src/routers/gameRouter.js
+++ backend/src/routers/gameRouter.js
@@ -1,6 +1,8 @@
 const express = require("express");
 
 const gameController = require("../controllers/gameController");
+const validateGameInfo = require("../middlewares/validators/gameValidator");
+const checkForExistingGame = require("../middlewares/gameMiddleware");
 
 const router = express.Router();
 
@@ -11,5 +13,9 @@ const router = express.Router();
  * TODO: add authentication wall...
  */
 router.get("/", gameController.getAll);
+
+router.put("/:id", validateGameInfo, gameController.editById);
+router.delete("/:id", gameController.remove);
+router.post("/", validateGameInfo, checkForExistingGame, gameController.post);
 
 module.exports = router;
