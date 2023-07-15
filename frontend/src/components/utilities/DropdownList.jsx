2b7bcc8b84e385e2307d520835c68aa0f4affdc0
Denis Dezest <denis.dezest@gmail.com>
2023-07-15 13:08:46 +0200

    frontend/src/components/utilities/Dropdown.jsx

--- frontend/src/components/utilities/DropdownList.jsx
+++ frontend/src/components/utilities/DropdownList.jsx
@@ -3,29 +3,47 @@ import PropTypes from "prop-types";
 
 // Helpers
 import capitalizeText from "../../helpers/capitalize";
+import filterTable from "../../helpers/filterTable";
 
-export default function DropdownList({ name, items }) {
+export default function DropdownList({
+  name,
+  items,
+  filterOptions,
+  selection,
+  onSelectionChange,
+  handleChange,
+}) {
+  const handleOnValueChange = (e, id) => {
+    // store current choice into parent dropdown state (allow dropwdown to be opened/closed without loosing selection)
+    onSelectionChange(id);
+    // send current selection info to grand-parent modal form
+    handleChange(e);
+  };
   return (
     <ul
       name={name}
-      className="h-48 overflow-y-auto px-3 pb-3 text-sm text-neutralLightest"
+      className="h-40 overflow-y-auto px-3 pb-3 text-sm text-neutralLightest"
       aria-labelledby="dropdownSearchButton"
     >
       {Array.isArray(items) && items.length > 0 ? (
-        items.map((item) => (
+        filterTable(items, "name", filterOptions).map((item) => (
           <li
             key={item.id}
             className="flex items-center rounded pl-2 hover:bg-gray-600"
           >
             <input
-              id={`checkbox-item-${item.id}`}
+              id={item.id}
               type="checkbox"
-              value=""
-              className="h-4 w-4 rounded border-neutral bg-gray-100 text-primary focus:outline-none"
+              // name={item.name}
+              name={name}
+              value={item.name}
+              checked={selection.find((el) => el.id === item.id).isSelected}
+              onChange={(event) => handleOnValueChange(event, item.id)}
+              className="h-4 w-4 rounded border-neutral bg-gray-100 focus:outline-none"
             />
             <label
-              htmlFor={`checkbox-item-${item.id}`}
-              className="ml-2 w-full rounded py-2 text-sm font-medium text-gray-300"
+              htmlFor={item.id}
+              className="ml-2 w-full rounded py-2 text-sm font-medium"
             >
               {capitalizeText(item.name)}
             </label>
@@ -46,4 +64,14 @@ DropdownList.propTypes = {
       name: PropTypes.string,
     })
   ).isRequired,
+  filterOptions: PropTypes.string.isRequired,
+  selection: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      isSelected: PropTypes.bool,
+    })
+  ).isRequired,
+  onSelectionChange: PropTypes.func.isRequired,
+  handleChange: PropTypes.func.isRequired,
 };
