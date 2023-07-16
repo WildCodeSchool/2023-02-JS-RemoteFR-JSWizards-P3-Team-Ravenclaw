218755fb0013049fcf71e7752c7e7848e0e68474
Denis Dezest <denis.dezest@gmail.com>
2023-07-16 11:09:37 +0200

    Created helper function to format the request body for video upload

---
+++ frontend/src/helpers/formatVideoBodyRequest.js
@@ -0,0 +1,27 @@
+const formatedTimestamp = () => {
+  const d = new Date();
+  const date = d.toISOString().split("T")[0];
+  const time = d.toTimeString().split(" ")[0];
+  return `${date} ${time}`;
+};
+
+export default function formatVideoBodyRequest(
+  videoInfo,
+  videoUrl,
+  thumbnailUrl
+) {
+  return {
+    title: videoInfo.title,
+    upload_date: formatedTimestamp(),
+    description: videoInfo.description,
+    slug: videoInfo.slug || "",
+    status: videoInfo.status || "online",
+    thumbnail: thumbnailUrl,
+    url_video: videoUrl,
+    is_promoted: videoInfo.isPromoted,
+    // eslint-disable-next-line no-nested-ternary
+    visibility: videoInfo.isPremium ? 2 : videoInfo.IsFreemium ? 1 : 0,
+    game_id: videoInfo.game.id,
+    language_id: videoInfo.language.id,
+  };
+}
