6d2779fa7661f3d20f1623f702c9ae315fbb3b91
Jeremy Dohin <arkantik.pro@gmail.com>
2023-07-18 19:17:48 +0200

    Updated user.jsx with 2 components

--- frontend/src/pages/User.jsx
+++ frontend/src/pages/User.jsx
@@ -1,7 +1,28 @@
-export default function User() {
+// Packages
+import PropTypes from "prop-types";
+import { useState } from "react";
+
+// Components
+import DashboardUser from "../components/DashboardUser";
+import TableFavorite from "../components/dashboard/favorite/TableFavorite";
+
+export default function User({ favorites, dashboard }) {
+  const [filterText, setFilterText] = useState("");
   return (
-    <div>
-      <div>Test</div>
-    </div>
+    <>
+      {dashboard && <DashboardUser />}
+      {favorites && (
+        <TableFavorite filterText={filterText} setFilterText={setFilterText} />
+      )}
+    </>
   );
 }
+
+User.defaultProps = {
+  favorites: null,
+  dashboard: null,
+};
+User.propTypes = {
+  favorites: PropTypes.bool,
+  dashboard: PropTypes.bool,
+};
