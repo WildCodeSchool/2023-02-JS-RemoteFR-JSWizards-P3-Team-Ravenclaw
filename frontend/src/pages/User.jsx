334fc11d6d03116cb7b6d570ece6ab96e3314e70
Jeremy Dohin <arkantik.pro@gmail.com>
2023-07-24 10:38:32 +0200

    Updated User page to only have 1 item, dashboard

--- frontend/src/pages/User.jsx
+++ frontend/src/pages/User.jsx
@@ -1,28 +1,16 @@
 // Packages
 import PropTypes from "prop-types";
-import { useState } from "react";
 
 // Components
 import DashboardUser from "../components/DashboardUser";
-import TableFavorite from "../components/dashboard/favorite/TableFavorite";
 
-export default function User({ favorites, dashboard }) {
-  const [filterText, setFilterText] = useState("");
-  return (
-    <>
-      {dashboard && <DashboardUser />}
-      {favorites && (
-        <TableFavorite filterText={filterText} setFilterText={setFilterText} />
-      )}
-    </>
-  );
+export default function User({ dashboard }) {
+  return dashboard && <DashboardUser />;
 }
 
 User.defaultProps = {
-  favorites: null,
   dashboard: null,
 };
 User.propTypes = {
-  favorites: PropTypes.bool,
   dashboard: PropTypes.bool,
 };
