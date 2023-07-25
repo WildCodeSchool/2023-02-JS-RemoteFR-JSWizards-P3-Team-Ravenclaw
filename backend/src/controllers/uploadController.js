0dd9eef3793ef16ed9c6cc4be5ef830d26208dc9
Denis Dezest <denis.dezest@gmail.com>
2023-07-25 19:30:57 +0200

    Updated upload router and controller to modify/delete game and video thumbnails

--- backend/src/controllers/uploadController.js
+++ backend/src/controllers/uploadController.js
@@ -28,4 +28,30 @@ const post = (req, res) => {
   );
 };
 
-module.exports = { post };
+const remove = (req, res) => {
+  const { thumbnail: thumbnailUrl } = req.body;
+
+  const isUploadFolder = thumbnailUrl.includes(FRONT_DEST);
+
+  // image is not in publick backend folder => do nothing
+  if (!isUploadFolder) {
+    res.status(200).json({
+      message: "Private files (frontend/assets) cannot be deleted...",
+    });
+  } else {
+    // retrieve thumbnail relative path in backend public folder
+    const fileName = `./public/uploads${req.url}/${thumbnailUrl.replace(
+      `${FRONT_DEST}uploads${req.url}/`,
+      ""
+    )}`;
+    // delete image
+    fs.unlink(fileName, (err) => {
+      if (err) throw err;
+    });
+    res.status(204).json({
+      message: "Thumbnail file successfully deleted!",
+    });
+  }
+};
+
+module.exports = { post, remove };
