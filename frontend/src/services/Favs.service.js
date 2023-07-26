da5dc036b7563153beaecaaf6b0b9701061f63e3
Jeremy Dohin <arkantik.pro@gmail.com>
2023-07-26 11:40:07 +0200

    Fixed api call errors in fav service

--- frontend/src/services/Favs.service.js
+++ frontend/src/services/Favs.service.js
@@ -4,7 +4,7 @@ const baseUrl = import.meta.env.VITE_BACKEND_URL;
 
 export const updateFavs = async (body) => {
   try {
-    const url = `${baseUrl}/userVideo`;
+    const url = `${baseUrl}/user-video`;
     const { data } = await axios.put(url, body);
     return data;
   } catch (error) {
@@ -14,7 +14,7 @@ export const updateFavs = async (body) => {
 
 export const getOneFav = async (userId, videoId) => {
   try {
-    const url = `${baseUrl}/userVideo?user_id=${userId}&video_id=${videoId}`;
+    const url = `${baseUrl}/user-video?user_id=${userId}&video_id=${videoId}`;
     const { data } = await axios.get(url);
     return data;
   } catch (error) {
@@ -24,7 +24,7 @@ export const getOneFav = async (userId, videoId) => {
 
 export const postFav = async (newFav) => {
   try {
-    const url = `${baseUrl}/userVideo`;
+    const url = `${baseUrl}/user-video`;
     const { data } = await axios.post(url, newFav);
     return data;
   } catch (error) {
@@ -34,17 +34,9 @@ export const postFav = async (newFav) => {
 
 export const deleteFav = async (data) => {
   try {
-    const url = `${baseUrl}/userVideo`;
+    const url = `${baseUrl}/user-video`;
     return axios.delete(url, data);
   } catch (error) {
     return { data: [] };
   }
-};
-
-export const unfavoriteVideo = (videoId, userId) => {
-  return axios.put(`${baseUrl}/user-video/`, {
-    user_id: userId,
-    video_id: videoId,
-    is_favorite: 0,
-  });
 };
